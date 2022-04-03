cdc275c2ba220d02fbb87fd218b03cd0e0640b3d
Caleb <caleb.gama10@gmail.com>
2022-04-03 12:53:32 -0300

    Tentativa1

---
+++ .terraform.lock.hcl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f7511132012e4dacfa6a973f5966d944a2489a6d8e82f8ffd5715acd22607b6d
+size 2157
---
+++ main.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2ce6d2b53e3ea3bccdfab51fd06045b8488e7d6f731cda1f318368f41f1d4c4e
+size 4511
